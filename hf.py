63785b2276bc37feea9a78a36f178f73bbefb7e5
Gaby <diazg1@mcmaster.ca>
2017-07-05 13:42:37 -0400

    Group initial code into functions

--- hf.py
+++ hf.py
@@ -2,137 +2,177 @@ import numpy as np
 from horton import *
 
 
-# Construct a molecule from scratch
-mol = IOData.from_file(context.get_fn('test/h2.xyz'))
+def ints_eval(xyz_file, basis):
+    """
+    Compute Gaussian integrals.
 
-# # Create a Gaussian basis set
-obasis = get_gobasis(mol.coordinates, mol.numbers, 'sto-3g')
+    Parameters:
+    -----------
+    xyz_file : molecule coordinate file 
+    basis : basis set 
+	
+    """
+     
+    mol = IOData.from_file(context.get_fn(xyz_file))
+    # Create a Gaussian basis set.
+    obasis = get_gobasis(mol.coordinates, mol.numbers, basis)
 
-# # Create a linalg factory
-lf = DenseLinalgFactory(obasis.nbasis)
+    # Create a linalg factory.
+    lf = DenseLinalgFactory(obasis.nbasis)
 
-# Basiset info
-#print "contraction coefficients"
+    # Compute One-electron Integrals.
+    olp = obasis.compute_overlap(lf) # overlap matrix
+    olp_m = olp._array
+    kin = obasis.compute_kinetic(lf)
+    kin_m = kin._array
+    na = obasis.compute_nuclear_attraction(mol.coordinates, mol.pseudo_numbers, lf)
+    na_m = na._array
+    er = obasis.compute_electron_repulsion(lf)
+    er_m = er._array
 
-# # Compute Gaussian integrals
-olp = obasis.compute_overlap(lf)
-olp_m = olp._array
-kin = obasis.compute_kinetic(lf)
-kin_m = kin._array
-na = obasis.compute_nuclear_attraction(mol.coordinates, mol.pseudo_numbers, lf)
-na_m = na._array
-er = obasis.compute_electron_repulsion(lf)
-er_m = er._array
-
-print "kin_m", kin_m
-print
-print "na_m", na_m
-print
-print "er_m", er_m
-
-X, U = np.linalg.eigh(olp_m)
-#print "Diagonalized matrix", X
-#print "Unitary matrix", U
-#XInv = np.linalg.inv(X)
-#Xinsqrt = np.power(Xinv, 0.5)
-Xinsqrt2 = np.power(X, -0.5)
-Xfin = Xinsqrt2*np.eye(2)
-
-#print "Xinsqrt2", Xinsqrt2
-#print Xfin
-#print U.T
-#print
-
-Ssqt = np.dot(U, np.dot(Xfin, U.T))
-
-# Fock initial guess
-H = kin_m + na_m
-Fzero = np.dot(Ssqt.T, np.dot(H, Ssqt))
-
-#print "core Hamiltonian", H
-#print "Initial Fock", Fzero
-#print
-
-# Coeff initial guess
-epsilon, Cp = np.linalg.eigh(Fzero)
-
-Czero = np.dot(Ssqt, Cp)
-#print "Cp", Cp
-#print
-#print "Czero nos", Czero
-#print
-#print "epsilon", epsilon
-
-occ = np.array([1, 0])
-
-# Initial density matrix
-D = np.zeros((2,2))
-for m in range(len(Czero)):
-    for v in range(len(Czero)):
-        for i in range(len(Czero)):
-            D[m,v] += occ[i]*Czero[m,i]*Czero[v, i]
-
-#D2 = np.dot(occ*Czero, Czero.T)
-
-#print "Density matrix", D
-#print
-#print "Density matrix", D2
-
-# New Fock matrix
-F = np.zeros((2,2))
-
-for m in range(2):
-    for v in range(2):
-        F[m, v] = H[m, v]
-        for p in range(2):
-            for s in range(2):
-                F[m, v] += D[p, s] * (2 * er_m[m,p,v,s] - er_m[m,v,p,s])
+    return {'olp':olp_m, 'kin':kin_m, 'na':na_m, 'er':er_m}
 
 
-#print "Fock", F
+def ortogonalize(olp_matrix):
+    """
+    Generate overlap ortogonalizing matrix
 
-# Energy
-Energy_0 = 0
+    Parameters:
+    -----------
+    olp_matrix : array
+                 Overlap Matrix
 
-for m in range(2):
-    for v in range(2):
-        Energy_0 += D[m,v]*(H[m,v] + F[m,v])
+    """
 
-enuc = 0.713176830593
-Etot = Energy_0 + enuc
-#print "Energy", Energy_0 + enuc
+    diag, unitary_m = np.linalg.eigh(olp_matrix)
+    diag_invsqr = np.power(diag, -0.5)
+    diag_invsqr_m = diag_invsqr * np.eye(2)
+    ortog_m = np.dot(unitary_m, np.dot(diag_invsqr_m, unitary_m.T))
+
+    #print "Diagonalized matrix", X
+    #print "Unitary matrix", U
+    #XInv = np.linalg.inv(X)
+    #Xinsqrt = np.power(Xinv, 0.5)
+    #print "Xinsqrt2", Xinsqrt2
+    #print Xfin
+    #print U.T
+    #print
+
+    return {'diag_mtx':diag, 'unitary_mtx':unitary_m, 'ortog_mtx':ortog_m}
 
 
-Etemp = 0
+def fock_guess(kin_m, na_m, ortog_m):
+    # Fock initial guess
+    H = kin_m + na_m
+    Fzero = np.dot(ortog_m.T, np.dot(H, ortog_m))
 
-while abs(Etot - Etemp) > 10e-6:
-    Etot = Etemp
-    # Transform Fock matrix
-    F = np.dot(Ssqt.T, np.dot(F, Ssqt))
+    #print "Initial Fock", Fzero
+    return Fzero, H
 
-    # diagonalize Fock matrix
-    epsilon, Cp = np.linalg.eigh(F)
 
-    # construct new eigenvector matrix
-    C = np.dot(Ssqt, Cp)
-
-    # form new density matrix
-    for m in range(len(C)):
-        for v in range(len(C)):
-            for i in range(len(C)):
-                D[m,v] += occ[i]*C[m,i]*C[v, i]
+def density_init_guess(Fzero, ortog_m, occ):
+    """
+    Construct initial guess density matrix
     
-    # new Fock matrix
+    """
+	
+    # Coefficient matrix initial guess
+    # Diagonalize the initial Fock_m
+    epsilon, Cp = np.linalg.eigh(Fzero)
+    
+    # Initial SCF eigenvecto matrix in the original basis
+    Czero = np.dot(ortog_m, Cp)
+    #print "Czero nos", Czero
+    #print "epsilon", epsilon
+
+    # Initial density matrix
+    D = np.zeros((2,2))
+    for m in range(len(Czero)):
+        for v in range(len(Czero)):
+            for i in range(len(Czero)):
+                D[m,v] += occ[i]*Czero[m,i]*Czero[v,i]
+   
+    # Otra via de obtener la misma D_inicial
+    #D2 = np.dot(occ*Czero, Czero.T)
+    
+    #print "Density matrix", D
+    #print "Density matrix", D2
+    return D
+
+
+def scf_iter(H, D, er_m, E_nuc):
+    """
+    Perform the SCF iterations
+    
+    """
+	
+    # Construct new Fock matrix
+	# this includes one and two electron terms
+    F = np.zeros((2,2))
+	
     for m in range(2):
         for v in range(2):
             F[m, v] = H[m, v]
             for p in range(2):
                 for s in range(2):
                     F[m, v] += D[p, s] * (2 * er_m[m,p,v,s] - er_m[m,v,p,s])
-    # New energy
+    #print "Fock", F
+
+    # Initialize electronic energy
+    Energy_0 = 0
+
     for m in range(2):
         for v in range(2):
-            Etemp += D[m,v]*(H[m,v] + F[m,v])
+            Energy_0 += D[m,v]*(H[m,v] + F[m,v])
 
-#print "HF Energy", Etemp
-print "END"
+    
+    E_tot = Energy_0 + E_nuc
+    print "Total Energy", Energy_0 + E_nuc
+
+    E_i = 0
+    while abs(E_i - E_tot) > 10e-6:
+        E_tot = E_i
+		
+        # Transform Fock matrix to the
+		# orthonormal basis
+        F = np.dot(Ssqt.T, np.dot(F, Ssqt))
+
+        # Diagonalize Fock matrix
+        epsilon, Cp = np.linalg.eigh(F)
+
+        # construct new eigenvector matrix
+        C = np.dot(Ssqt, Cp)
+
+        # Form new density matrix
+        for m in range(len(C)):
+            for v in range(len(C)):
+                for i in range(len(C)):
+                    D[m,v] += occ[i]*C[m,i]*C[v, i]
+        
+        # New Fock matrix
+        for m in range(2):
+            for v in range(2):
+                F[m, v] = H[m, v]
+                for p in range(2):
+                    for s in range(2):
+                        F[m, v] += D[p, s] * (2 * er_m[m,p,v,s] - er_m[m,v,p,s])
+        # New energy
+        for m in range(2):
+            for v in range(2):
+                E_i += D[m,v]*(H[m,v] + F[m,v])
+
+    print "SCF done. HF Energy is ", E_i
+
+
+if __name__ == "__main__":
+
+    # Construct a molecule from scratch
+    ints_eval('test/h2.xyz', 'sto-3g')
+    
+    ort_overlap(olp_matrix)
+	
+    # Occupation matrix
+    occ = np.array([1, 0])
+	
+    # Nuclear Repulsion Energy
+    E_nuc = 0.713176830593
